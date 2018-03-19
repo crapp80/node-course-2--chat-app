5402d94f9be980dd132a7465a76f51050f8aa673
crapp80 <carsten.rapp@icloud.com>
2018-03-19 17:30:47 +0100

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -36,11 +36,13 @@ socket.on('newLocationMessage', function (message) {
 $('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  const messageTextbox = $('input[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: $('input[name=message]').val(),
+    text: messageTextbox.val(),
   }, function () {
-
+    messageTextbox.val('');
   });
 });
 
@@ -49,12 +51,17 @@ locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   return navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude,
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   });
 });
