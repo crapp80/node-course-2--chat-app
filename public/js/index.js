676c4a16568220e00c3da85e0087c352204379c6
crapp80 <carsten.rapp@icloud.com>
2018-03-26 22:05:55 +0200

    Format timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,6 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-undef */
 
-
 const socket = io();
 
 socket.on('connect', function () {
@@ -15,19 +14,19 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
+  const formattedTime = moment(message.createdAt).format('h:mm a');
   const li = $('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
-  console.log('newLocationMessage', message);
+  const formattedTime = moment(message.createdAt).format('h:mm a');
   const li = $('<li></li>');
   const a = $('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   $('#messages').append(li);
