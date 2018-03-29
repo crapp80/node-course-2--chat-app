dd1819fd132fff7e7ae3fe1f52d9daf8158c23b6
crapp80 <carsten.rapp@icloud.com>
2018-03-29 22:44:53 +0200

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,22 @@
 
 const socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  const messages = $('#messages');
+  const newMessage = messages.children('li:last-child');
+  // Heights
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -23,6 +39,7 @@ socket.on('newMessage', function (message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -35,6 +52,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 $('#message-form').on('submit', function (e) {
